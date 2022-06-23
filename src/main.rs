0060c8be50c28c7d4c9427b92e61c1218d5e815b
LZ <lizong20001204@gmail.com>
2022-06-23 16:10:26 +0800

    feat: add config file parse
    Switch the configuration mode and use the configuration file for processing.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-use std::env;
+extern crate serde_json;
+
+use std::{env, process};
 use std::fs;
+use std::fs::File;
 use std::path::Path;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::process::Command;
+use serde::{Serialize, Deserialize};
 
 fn main() {
-    let path:Vec<String> = env::args().collect();
+    let path: Vec<String> = env::args().collect();
     let path = &path[1].as_str();
-    // 验证路径是否存在
-    if !Path::new(&path).exists() {
-        panic!("Path does not exist.");
+    let file = Path::new(&path);
+    // 验证路径是否存在且是否是个文件
+    if !file.exists() || !file.is_file() {
+        panic!("config file does not exist.");
     }
+
+    // 处理并检查配置文件
+    let conf = parse_config(path.to_string());
+    inspect_config(conf);
+
     // 监听对应端口
-    let listener = TcpListener::bind("127.0.0.1:12004").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:12004").expect("The port is occupied.");
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
@@ -20,7 +31,30 @@ fn main() {
     }
 }
 
-/*处理请求*/
+// 转换配置
+fn parse_config(path: String) -> Config {
+    let config_file = File::open(path).unwrap();
+    serde_json::from_reader(config_file).unwrap()
+}
+
+// 检查配置
+fn inspect_config(conf: Config) {
+    for proxy in conf.proxy_group {
+        if proxy.port == 0u32 || proxy.port > 65535u32 {
+            prompt_and_exit("Checking Port Settings.");
+        }
+    }
+}
+
+// 提示并退出
+fn prompt_and_exit(msg: &str) {
+    let _ = Command::new("bash")
+        .args(&["-c", format!("echo {}", msg).as_str()])
+        .spawn();
+    process::exit(1);
+}
+
+// 处理请求
 fn handle_connection(path: String, mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
@@ -42,7 +76,7 @@ fn handle_connection(path: String, mut stream: TcpStream) {
                     c.len(),
                     c
                 )
-            },
+            }
             Err(_c) => {
                 let not_found_page = "<h1>404</h1>";
                 format!(
@@ -54,4 +88,19 @@ fn handle_connection(path: String, mut stream: TcpStream) {
         };
         stream.write(content.as_bytes()).unwrap();
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    proxy_group: Vec<Proxy>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Proxy {
+    port: u32,
+    context: String,
+    path: String,
+    index: String,
+    timeout: u16,
+    cache: bool,
 }
